a777aac60877322c87c66524acbad43528069136
1226338-glitch <1226338@virhec.soy.pe>
2026-04-15 14:33:04 -0500

    Create TransactionDAO.java with methods for transactions

---
+++ src/main/java/com/sistemasfinancieros/dao/TransactionDAO.java
@@ -0,0 +1,21 @@
+package com.sistemasfinancieros.dao;
+
+import java.util.Date;
+import java.util.List;
+
+public class TransactionDAO {
+
+    public List<Transaction> getTransactionsByAccount(String accountId) {
+        // Implementation here
+        return null;
+    }
+
+    public void createTransaction(Transaction transaction) {
+        // Implementation here
+    }
+
+    public List<Transaction> getTransactionsByDateRange(Date startDate, Date endDate) {
+        // Implementation here
+        return null;
+    }
+}
