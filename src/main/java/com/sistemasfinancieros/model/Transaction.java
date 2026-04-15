764116c08af80699a0167b3d39a5eb38ce653dcc
1226338-glitch <1226338@virhec.soy.pe>
2026-04-15 14:34:27 -0500

    Created Transaction.java model class

---
+++ src/main/java/com/sistemasfinancieros/model/Transaction.java
@@ -0,0 +1,70 @@
+package com.sistemasfinancieros.model;
+
+import java.util.Date;
+
+public class Transaction {
+    private String transactionId;
+    private String accountId;
+    private String tipo;
+    private double monto;
+    private Date fecha;
+    private String descripcion;
+    private String estado;
+
+    // Getters and Setters
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
